4d6bbe161a5c2d8a7471783c5438313dbec5bf91
Viktor Varenik <yavarenikya@gmail.com>
2025-06-02 01:49:19 +0300

    feat: added prettier and configured linter

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,13 +1,16 @@
 "use client"
 
-import Image from "next/image";
-import LogoLight from "@/app/images/logo-light.svg";
-import LogoDark from "@/app/images/logo-dark.svg";
-import {useTheme} from "next-themes";
+import Image from "next/image"
+import LogoLight from "@/app/images/logo-light.svg"
+import LogoDark from "@/app/images/logo-dark.svg"
+import { useTheme } from "next-themes"
 
 export default function Logo() {
-    const theme = useTheme();
-    return (
-        <Image src={theme.resolvedTheme != "dark" ? LogoLight : LogoDark} alt="ShareBox logo"/>
-    );
+  const theme = useTheme()
+  return (
+    <Image
+      src={theme.resolvedTheme != "dark" ? LogoLight : LogoDark}
+      alt="ShareBox logo"
+    />
+  )
 }
