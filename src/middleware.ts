b90b4d9ea096fc1c0525e3409ca11596fa5f3ef8
Viktor Varenik <yavarenikya@gmail.com>
2025-06-09 21:40:31 +0300

    feat: basic jwt auth logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getPreferredLocale } from "./i18n/getPreferredLocale";
 import { languages } from "@/i18n/settings";
+import { verifyJwtToken } from "@/auth";
 
-export function middleware(req: NextRequest) {
-    const { nextUrl } = req;
+const AUTH_PAGE = "/auth";
+const NOT_REQUIRED_AUTH_PAGES = [
+    "/",
+    "/en",
+    "/uk",
+    "/auth",
+    "/en/auth",
+    "/uk/auth",
+];
+
+const isNotRequireAuth = (url: string) =>
+    NOT_REQUIRED_AUTH_PAGES.some((page) => page == url);
+
+export async function middleware(req: NextRequest) {
+    const { nextUrl, cookies } = req;
+    const { value: token } = cookies.get("token") ?? { value: null };
     const newUrl = nextUrl.clone();
     const pathname = newUrl.pathname;
     const res = NextResponse.rewrite(newUrl);
+
+    const isAuthRequired = !isNotRequireAuth(nextUrl.pathname);
+
+    if (isAuthRequired) {
+        // Verify auth
+        const hasVerifiedToken = token && (await verifyJwtToken(token));
+        if (!hasVerifiedToken) {
+            newUrl.pathname = AUTH_PAGE;
+            return NextResponse.redirect(newUrl);
+        }
+    }
 
     const localeInPath = languages.find(
         (locale) =>
